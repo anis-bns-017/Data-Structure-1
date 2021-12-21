b57e231cfdbb9ed02173b65cd401b0070110c219
MD Nasir Uddin Rubel <96114807+nurubel1819@users.noreply.github.com>
2021-12-21 16:27:25 +0600

    just simplification this code

--- Algorithm/sort/bubble sort.cpp
+++ Algorithm/sort/bubble sort.cpp
@@ -2,26 +2,15 @@
 using namespace std;
 int main()
 {
-    int a=10,b=20;
-
     int array_size;
     cout<< "Enter array size :";
     cin>>array_size;
     int arr[array_size];
     cout<< "Enter "<< array_size<< " element with space :";
-    for(int i=0; i<array_size; i++)
-        cin>>arr[i];
-    for(int i = 0; i<array_size; i++)
-    {
-        for(int j=0; j<array_size-i; j++)
-        {
-            if(arr[j]>arr[j+1])
-                swap(arr[j],arr[j+1]);
-        }
-    }
+    for(int i=0; i<array_size; i++) cin>>arr[i];
+    for(int i = 0; i<array_size; i++) for(int j=1; j<array_size-i; j++) if(arr[j-1]>arr[j]) swap(arr[j-1],arr[j]);
     cout<< "After sort element is here : ";
-    for(int i=0; i<array_size; i++)
-        cout<< arr[i]<< " ";
+    for(auto i : arr) cout<<i<< " ";
     cout<<endl;
     return 0;
 }
