a385e6ba00c1cedd80edbd468546ddbbb74cef9d
MD Nasir Uddin Rubel <96114807+nurubel1819@users.noreply.github.com>
2022-04-13 16:26:15 +0600

    Update floyd_warshall_algorithm.cpp

--- Algorithm/Graph/floyd_warshall_algorithm.cpp
+++ Algorithm/Graph/floyd_warshall_algorithm.cpp
@@ -7,9 +7,9 @@ int main()
     cout << "Enter vertex and edges number with space :";
     cin >> vertex >> edge;
     int matrix_0[vertex + 1][vertex + 1];
-    for (int i = 0; i <= vertex; i++)
+    for (int i = 0; i < vertex; i++)
     {
-        for (int j = 0; j <= vertex; j++)
+        for (int j = 0; j < vertex; j++)
         {
             if (i == j)
                 matrix_0[i][j] = 0;
@@ -21,25 +21,23 @@ int main()
     cout << "Formate is :(vertex vertex value ) : 2 4 7" << endl;
     for (int i = 0; i < edge; i++)
     {
-        int x, y, z;
-        cin >> x >> y >> z;
-        matrix_0[x][y] = z;
-    }
-    /// if vertex start 1
-    for (int k = 1; k <= vertex; k++)
-    {
-        for (int i = 1; i <= vertex; i++)
-        {
+        int vertex_1,vertex_2,value;
+        cin>>vertex_1>>vertex_2>>value;
+        vertex_1=vertex_1-1;
+        vertex_2=vertex_2-1;
+        matrix_0[vertex_1][vertex_2] = value;
 
-            for (int j = 1; j <= vertex; j++)
-            {
-                matrix_0[i][j] = min(matrix_0[i][j], (matrix_0[i][k] + matrix_0[k][j]));
-            }
-        }
+        /// when vertex or node is define cher like a,b,c...
+        /*
+        char char_vertex_1,char_vertex_2;
+        cin>>char_vertex_1>>char_vertex_2>>value;
+        vertex_1=(char_vertex_1-'a'); /// it graph start with a
+        vertex_2=(char_vertex_2-'a');
+        matrix_0[vertex_1][vertex_2] = value;
+        */
     }
-
-    /// if vertex start is 0
-    /*
+    
+    
     for(int k=0; k<vertex; k++)
     {
         for(int i=0; i<vertex; i++)
@@ -51,15 +49,26 @@ int main()
             }
         }
     }
-    */
+    
 
     int t = 50;
     while (t--)
     {
-        int a, b;
+        char start_char,end_char;
+        int start_node,end_node;
         cout << "Enter start and end vertex : ";
-        cin >> a >> b;
-        cout << "Sortage distance is : " << matrix_0[a][b] << endl;
+        cin>>start_node>>end_node;
+        start_node=start_node-1;
+        end_node=end_node-1;
+
+        ///if graph have char vertex
+        /*
+        cin>>start_char>>end_char;
+        start_node=(start_char-'a');
+        end_node=(end_char-'a');
+        */
+
+        cout << "Sortage distance is : " << matrix_0[start_node][end_node] << endl;
     }
 
     return 0;
